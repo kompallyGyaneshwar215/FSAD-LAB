687a5ed7e54ff54f305e6fd48ec96be61bd57163
kompally Gyaneshwar <gyaneshwarkompally@gmail.com>
2026-04-17 20:17:57 +0530

    Add files via upload

---
+++ Lab 10/src/App.jsx
@@ -0,0 +1,10 @@
+import StudentList from "./components/StudentList"; 
+function App() { 
+return ( 
+<div> 
+<h1>Student Management System</h1> 
+<StudentList /> 
+</div> 
+); 
+} 
+export default App; 
